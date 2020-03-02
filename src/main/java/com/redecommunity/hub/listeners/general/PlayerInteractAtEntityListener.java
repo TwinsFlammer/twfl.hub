97dc36e4136299b5f7337aa76f49032964006f80
Gutyerrez <srgutyerrez@gmail.com>
2020-03-02 13:27:32 -0300

    Create PlayerInteractAtEntityListener.java

---
+++ src/main/java/com/redecommunity/hub/listeners/general/PlayerInteractAtEntityListener.java
@@ -0,0 +1,25 @@
+package com.redecommunity.hub.listeners.general;
+
+import com.redecommunity.common.shared.permissions.group.GroupNames;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PlayerInteractAtEntityListener implements Listener {
+    @EventHandler
+    public void onInteract(PlayerInteractAtEntityEvent event) {
+        Player player = event.getPlayer();
+
+        User user = UserManager.getUser(player.getUniqueId());
+
+        if (user.hasGroup(GroupNames.MANAGER)) return;
+
+        event.setCancelled(true);
+    }
+}
