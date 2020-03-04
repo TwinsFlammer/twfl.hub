f90dc48eac0e48dbc98811be621de7b468a7e960
Gutyerrez <srgutyerrez@gmail.com>
2020-03-04 14:51:47 -0300

    Update PlayerJoinListener.java

--- src/main/java/com/redecommunity/hub/listeners/general/PlayerJoinListener.java
+++ src/main/java/com/redecommunity/hub/listeners/general/PlayerJoinListener.java
@@ -4,10 +4,13 @@ import com.redecommunity.api.spigot.scoreboard.CustomBoard;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.permissions.user.manager.UserManager;
 import com.redecommunity.hub.scoreboard.manager.ScoreboardManager;
+import com.redecommunity.hub.selector.item.SelectorItem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 /**
  * Created by @SrGutyerrez
@@ -22,5 +25,19 @@ public class PlayerJoinListener implements Listener {
         CustomBoard customBoard = ScoreboardManager.create(user);
 
         customBoard.send(player);
+
+        ItemStack selectorItem = new SelectorItem()
+                .build();
+
+        PlayerInventory playerInventory = player.getInventory();
+
+        playerInventory.clear();
+
+        playerInventory.setItem(
+                4,
+                selectorItem
+        );
+
+        playerInventory.setHeldItemSlot(4);
     }
 }
