b1c885fd20dc1cee83d062d058ef56f6e310ef1c
Gutyerrez <srgutyerrez@gmail.com>
2020-03-04 14:52:29 -0300

    Update ServerInfo.java

--- src/main/java/com/redecommunity/hub/selector/data/ServerInfo.java
+++ src/main/java/com/redecommunity/hub/selector/data/ServerInfo.java
@@ -1,5 +1,6 @@
 package com.redecommunity.hub.selector.data;
 
+import com.redecommunity.api.spigot.hologram.CustomHologram;
 import com.redecommunity.api.spigot.inventory.item.CustomItem;
 import com.redecommunity.common.shared.language.enums.Language;
 import com.redecommunity.common.shared.permissions.user.data.User;
@@ -10,6 +11,7 @@ import com.redecommunity.hub.selector.exception.UnknownServerException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
@@ -23,10 +25,7 @@ public class ServerInfo {
 
     @Getter
     @Setter
-    private Integer npcId;
-
-    @Getter
-    private final Integer slot, serverId;
+    private Integer npcId, slot, serverId;
 
     private Boolean active;
 
@@ -36,8 +35,25 @@ public class ServerInfo {
     @Getter
     private String description;
 
+    @Getter
+    @Setter
+    private CustomHologram hologram;
+
+    public Server getServer() {
+        return ServerManager.getServer(this.serverId);
+    }
+
     public Boolean isActive() {
         return this.active;
+    }
+
+    public Boolean isSimilar(ServerInfo serverInfo) {
+        return serverInfo.npcId.equals(this.npcId)
+                && serverInfo.slot.equals(this.slot)
+                && serverInfo.serverId.equals(this.serverId)
+                && serverInfo.active.equals(this.active)
+                && serverInfo.material.equals(this.material)
+                && serverInfo.description.equals(this.description);
     }
 
     public CustomItem getCustomItem() throws UnknownServerException {
@@ -48,7 +64,19 @@ public class ServerInfo {
         if (server == null)
             throw new UnknownServerException("Não foi possível localizar esse servidor.");
 
-        String[] lore = this.description.split("\\\\n");
+        String[] lore = StringUtils.replaceEach(
+                this.description,
+                new String[]{
+                        "{online}",
+                        "{slots}",
+                        "{status}"
+                },
+                new String[]{
+                        server.getPlayerCount().toString(),
+                        server.getSlots().toString(),
+                        server.getStatusString()
+                }
+        ).split("\\\\n");
 
         customItem.name("§a" + server.getDisplayName())
                 .lore(lore)
@@ -76,5 +104,14 @@ public class ServerInfo {
                 });
 
         return customItem;
+    }
+
+    public void update(ServerInfo serverInfo) {
+        this.npcId = serverInfo.npcId;
+        this.slot = serverInfo.slot;
+        this.serverId = serverInfo.serverId;
+        this.active = serverInfo.active;
+        this.material = serverInfo.material;
+        this.description = serverInfo.description;
     }
 }
