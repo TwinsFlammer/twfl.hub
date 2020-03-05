5cbab46c76195c2468f759a0b5636202bca2492f
Gutyerrez <srgutyerrez@gmail.com>
2020-03-04 21:53:37 -0300

    Create NPCMethods.java

---
+++ src/main/java/com/redecommunity/hub/selector/NPCMethods.java
@@ -0,0 +1,21 @@
+package com.redecommunity.hub.selector;
+
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.NameTagVisibility;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.Team;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class NPCMethods {
+    public static void hide(NPC npc, Player player) {
+        Scoreboard scoreboard = player.getScoreboard();
+        Team team = scoreboard.getTeam("ZZZ") == null ? scoreboard.registerNewTeam("ZZZ") : scoreboard.getTeam("ZZZ");
+        team.setNameTagVisibility(NameTagVisibility.NEVER);
+        team.setCanSeeFriendlyInvisibles(false);
+        team.setPrefix("§8[NPC] ");
+        team.addEntry(npc.getEntity().getName());
+    }
+}
