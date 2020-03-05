6010583b0b41b3f0a949cd03c27cbe3f2a1c0ddc
Gutyerrez <srgutyerrez@gmail.com>
2020-03-04 21:50:55 -0300

    Create NPCListener.java

---
+++ src/main/java/com/redecommunity/hub/selector/listener/NPCListener.java
@@ -0,0 +1,60 @@
+package com.redecommunity.hub.selector.listener;
+
+import com.redecommunity.api.spigot.hologram.data.CustomHologram;
+import com.redecommunity.common.shared.server.data.Server;
+import com.redecommunity.hub.selector.NPCMethods;
+import com.redecommunity.hub.selector.data.ServerInfo;
+import com.redecommunity.hub.selector.manager.ServerInfoManager;
+import net.citizensnpcs.api.event.NPCDespawnEvent;
+import net.citizensnpcs.api.event.NPCPushEvent;
+import net.citizensnpcs.api.event.NPCSpawnEvent;
+import net.citizensnpcs.api.npc.NPC;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class NPCListener implements Listener {
+    @EventHandler
+    public void onSpawn(NPCSpawnEvent event) {
+        NPC npc = event.getNPC();
+
+        Bukkit.getOnlinePlayers().forEach(player -> NPCMethods.hide(npc, player));
+
+        ServerInfo serverInfo = ServerInfoManager.getServerInfo(npc);
+
+        if (serverInfo == null) return;
+
+        serverInfo.spawn(npc);
+    }
+
+    @EventHandler
+    public void onDespawn(NPCDespawnEvent event) {
+        NPC npc = event.getNPC();
+
+        ServerInfo serverInfo = ServerInfoManager.getServerInfo(npc);
+
+        if (serverInfo == null) return;
+
+        serverInfo.despawn();
+    }
+
+    @EventHandler
+    public void onTeleport(NPCPushEvent event) {
+        NPC npc = event.getNPC();
+
+        Bukkit.getOnlinePlayers().forEach(player -> NPCMethods.hide(npc, player));
+
+        ServerInfo serverInfo = ServerInfoManager.getServerInfo(npc);
+
+        if (serverInfo == null) return;
+
+        serverInfo.teleportHologram(npc);
+    }
+}
