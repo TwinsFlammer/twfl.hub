d5ca220a046cbc8e0d988d6d598acf581e5ef7b4
Gutyerrez <srgutyerrez@gmail.com>
2020-03-08 20:57:48 -0300

    Update SelectorInventory.java

--- src/main/java/com/redecommunity/hub/selector/inventory/SelectorInventory.java
+++ src/main/java/com/redecommunity/hub/selector/inventory/SelectorInventory.java
@@ -2,8 +2,15 @@ package com.redecommunity.hub.selector.inventory;
 
 import com.redecommunity.api.spigot.inventory.CustomPaginateInventory;
 import com.redecommunity.api.spigot.inventory.item.CustomItem;
+import com.redecommunity.common.shared.permissions.user.data.User;
+import com.redecommunity.common.shared.server.data.Server;
+import com.redecommunity.common.shared.server.enums.ServerType;
+import com.redecommunity.common.shared.server.manager.ServerManager;
 import com.redecommunity.hub.selector.exception.UnknownServerException;
 import com.redecommunity.hub.selector.manager.ServerInfoManager;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 
 /**
  * Created by @SrGutyerrez
@@ -27,6 +34,45 @@ public class SelectorInventory extends CustomPaginateInventory {
                     } catch (UnknownServerException exception) {
                         exception.printStackTrace();
                     }
+                });
+    }
+
+    public SelectorInventory(ServerType serverType, User user) {
+        super(
+                "Selecione o " + serverType.getCategoryName(),
+                3,
+                "XXXXXXXXX",
+                "XOXXOXXOX",
+                "XXXXXXXXX"
+        );
+
+        ServerManager.getServers()
+                .stream()
+                .filter(Server::isLobby)
+                .forEach(server -> {
+                    Server userServer = user.getServer();
+
+                    CustomItem customItem = new CustomItem(Material.INK_SACK)
+                            .data(userServer.isSimilar(server) ? 13 : server.isOnline() ? 10 : 8)
+                            .name((userServer.isSimilar(server) ? "§e" : server.isOnline() ? "§a" : "§c" ) + ChatColor.stripColor(server.getDisplayName()))
+                            .lore(
+                                    String.format(
+                                            "§fJogadores: §7%d/%d",
+                                            server.getPlayerCount(),
+                                            server.getSlots()
+                                    ),
+                                    "",
+                                    userServer.isSimilar(server) ? "§eVocê já está conectado a este saguão!" : server.isOnline() ? "§aClique para conetar!" : "§cEste saguão está offline."
+                            )
+                            .enchant(userServer.isSimilar(server) ? Enchantment.DURABILITY : null, 1)
+                            .hideAttributes()
+                            .onClick(event -> {
+                                if (userServer.isSimilar(server)) return;
+
+                                if (server.isOnline()) user.connect(server);
+                            });
+
+                    this.addItem(customItem);
                 });
     }
 }
