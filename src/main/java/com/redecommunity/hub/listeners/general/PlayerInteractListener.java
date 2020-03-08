484bb3806ec25df9dcf1000df241a00acf65db1c
Gutyerrez <srgutyerrez@gmail.com>
2020-03-08 20:58:45 -0300

    Update PlayerInteractListener.java

--- src/main/java/com/redecommunity/hub/listeners/general/PlayerInteractListener.java
+++ src/main/java/com/redecommunity/hub/listeners/general/PlayerInteractListener.java
@@ -1,16 +1,25 @@
 package com.redecommunity.hub.listeners.general;
 
+import com.redecommunity.api.spigot.preference.event.PreferenceStateChangeEvent;
+import com.redecommunity.common.shared.cooldown.data.Cooldown;
+import com.redecommunity.common.shared.cooldown.manager.CooldownManager;
 import com.redecommunity.common.shared.permissions.group.GroupNames;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.permissions.user.manager.UserManager;
+import com.redecommunity.common.shared.preference.Preference;
+import com.redecommunity.common.shared.server.enums.ServerType;
+import com.redecommunity.hub.item.LobbyItem;
 import com.redecommunity.hub.selector.inventory.SelectorInventory;
-import com.redecommunity.hub.selector.item.SelectorItem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by @SrGutyerrez
@@ -24,15 +33,59 @@ public class PlayerInteractListener implements Listener {
 
         if (!user.hasGroup(GroupNames.MANAGER)) event.setCancelled(true);
 
-        ItemStack inHand = player.getItemInHand(),
-                selectorItem = new SelectorItem().build();
+        Action action = event.getAction();
 
-        if (inHand.isSimilar(selectorItem)) {
-            player.openInventory(
-                    new SelectorInventory()
-                            .build()
-            );
-            return;
+        if (action != Action.RIGHT_CLICK_AIR && action != Action.RIGHT_CLICK_BLOCK) return;
+
+        PlayerInventory playerInventory = player.getInventory();
+
+        ItemStack inHand = playerInventory.getItemInHand();
+
+        LobbyItem lobbyItem = LobbyItem.getLobbyItem(inHand);
+
+        if (lobbyItem == null) return;
+
+        switch (lobbyItem) {
+            case VISIBILITY_ON:
+            case VISIBILITY_OFF: {
+                LobbyItem newLobbyItem = lobbyItem.getParent();
+
+                PreferenceStateChangeEvent preferenceStateChangeEvent = new PreferenceStateChangeEvent(
+                        user,
+                        Preference.VISIBILITY
+                );
+
+                preferenceStateChangeEvent.run();
+
+                if (preferenceStateChangeEvent.isCancelled()) return;
+
+                if (CooldownManager.inCooldown(user, Preference.VISIBILITY)) return;
+
+                CooldownManager.startCooldown(
+                        user,
+                        TimeUnit.SECONDS.toMillis(5),
+                        Preference.VISIBILITY
+                );
+
+                user.togglePreference(Preference.VISIBILITY, user.isEnabled(Preference.VISIBILITY));
+
+                newLobbyItem.give(player);
+                return;
+            }
+            case SERVER_SELECTOR: {
+                player.openInventory(
+                        new SelectorInventory()
+                                .build()
+                );
+                return;
+            }
+            case LOBBY: {
+                player.openInventory(
+                        new SelectorInventory(ServerType.LOBBY, user)
+                                .build()
+                );
+                return;
+            }
         }
     }
 }
