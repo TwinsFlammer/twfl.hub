ab2701cd565080a3c41f44f3a32c604d45a14eb3
Gutyerrez <srgutyerrez@gmail.com>
2020-03-08 23:07:23 -0300

    Update PreferenceStateChangeListener.java

--- src/main/java/com/redecommunity/hub/item/listener/PreferenceStateChangeListener.java
+++ src/main/java/com/redecommunity/hub/item/listener/PreferenceStateChangeListener.java
@@ -1,30 +1,31 @@
 package com.redecommunity.hub.item.listener;
 
 import com.redecommunity.api.spigot.preference.event.PreferenceStateChangeEvent;
+import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.preference.Preference;
-import com.redecommunity.hub.item.LobbyItem;
+import com.redecommunity.hub.item.enums.LobbyItem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import java.util.Arrays;
 
 /**
  * Created by @SrGutyerrez
  */
 public class PreferenceStateChangeListener implements Listener {
-    @EventHandler
+    @EventHandler(ignoreCancelled = false)
     public void onChange(PreferenceStateChangeEvent event) {
+        if (event.isCancelled()) return;
+
         Player player = event.getPlayer();
 
         Preference preference = event.getPreference();
 
         if (preference != Preference.VISIBILITY) return;
 
-        Arrays.stream(LobbyItem.values())
-                .filter(lobbyItem -> lobbyItem.name().startsWith("VISIBILITY_"))
-                .forEach(lobbyItem -> {
-                    lobbyItem.give(player);
-                });
+        User user = event.getUser();
+
+        LobbyItem lobbyItem = user.isEnabled(preference) ? LobbyItem.VISIBILITY_ON : LobbyItem.VISIBILITY_OFF;
+
+        lobbyItem.give(player);
     }
 }
