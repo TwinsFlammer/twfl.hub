48d908a3fe08c28ebc08aa384c4a840ec0af7169
Gutyerrez <srgutyerrez@gmail.com>
2020-04-16 06:59:59 -0300

    Update PreferenceStateChangeListener.java

--- src/main/java/com/redecommunity/hub/item/listener/PreferenceStateChangeListener.java
+++ src/main/java/com/redecommunity/hub/item/listener/PreferenceStateChangeListener.java
@@ -1,12 +1,15 @@
 package com.redecommunity.hub.item.listener;
 
 import com.redecommunity.api.spigot.preference.event.PreferenceStateChangeEvent;
+import com.redecommunity.common.shared.cooldown.manager.CooldownManager;
 import com.redecommunity.common.shared.permissions.user.data.User;
 import com.redecommunity.common.shared.preference.Preference;
 import com.redecommunity.hub.item.enums.LobbyItem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by @SrGutyerrez
@@ -27,5 +30,11 @@ public class PreferenceStateChangeListener implements Listener {
         LobbyItem lobbyItem = user.isEnabled(preference) ? LobbyItem.VISIBILITY_ON : LobbyItem.VISIBILITY_OFF;
 
         lobbyItem.give(player);
+
+        CooldownManager.startCooldown(
+                user,
+                TimeUnit.SECONDS.toMillis(5),
+                preference
+        );
     }
 }
