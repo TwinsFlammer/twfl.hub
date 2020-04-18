069536f3d91ea8e3eb96963c5b45f5c1bff77963
Gutyerrez <srgutyerrez@gmail.com>
2020-04-18 01:05:48 -0300

    Update EntityChangeBlockListener.java

--- src/main/java/com/redecommunity/hub/listeners/general/EntityChangeBlockListener.java
+++ src/main/java/com/redecommunity/hub/listeners/general/EntityChangeBlockListener.java
@@ -8,6 +8,8 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.block.LeavesDecayEvent;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 
 /**
@@ -35,12 +37,22 @@ public class EntityChangeBlockListener implements Listener {
     }
 
     @EventHandler
+    public void onLeavesDecay(LeavesDecayEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
     public void onChange(BlockFromToEvent event) {
         event.setCancelled(true);
     }
 
     @EventHandler
     public void onFade(BlockFadeEvent event) {
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockSpread(BlockSpreadEvent event) {
         event.setCancelled(true);
     }
 }
