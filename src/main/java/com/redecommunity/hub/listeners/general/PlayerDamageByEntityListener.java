682a287dc8214f56c8217167cd92bf8ec39b39d3
Gutyerrez <srgutyerrez@gmail.com>
2020-04-18 03:48:26 -0300

    Update PlayerDamageByEntityListener.java

--- src/main/java/com/redecommunity/hub/listeners/general/PlayerDamageByEntityListener.java
+++ src/main/java/com/redecommunity/hub/listeners/general/PlayerDamageByEntityListener.java
@@ -1,7 +1,5 @@
 package com.redecommunity.hub.listeners.general;
 
-import com.redecommunity.api.spigot.util.JSONText;
-import com.redecommunity.common.shared.Common;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
@@ -20,22 +18,7 @@ public class PlayerDamageByEntityListener implements Listener {
 
         if (entity instanceof Player) event.setCancelled(true);
 
-        if (entity instanceof ItemFrame) {
-            Entity damager = event.getDamager();
-
-            Player player = (Player) damager;
-
-            new JSONText()
-                    .text("\n")
-                    .next()
-                    .text("§aClique ")
-                    .next()
-                    .text("§lAQUI")
-                    .clickOpenURL(Common.SERVER_URL)
-                    .next()
-                    .text("§r§apara acessar o site!")
-                    .next()
-                    .send(player);
-        }
+        if (entity instanceof ItemFrame)
+            event.setCancelled(true);
     }
 }
