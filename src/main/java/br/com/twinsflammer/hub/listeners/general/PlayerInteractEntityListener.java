1f281d292801007bedf06c2364264ff155124e11
Gutyerrez <srgutyerrez@gmail.com>
2020-05-27 07:44:28 -0300

    Update PlayerInteractEntityListener.java

--- src/main/java/br/com/twinsflammer/hub/listeners/general/PlayerInteractEntityListener.java
+++ src/main/java/br/com/twinsflammer/hub/listeners/general/PlayerInteractEntityListener.java
@@ -1,7 +1,11 @@
 package br.com.twinsflammer.hub.listeners.general;
 
+import br.com.twinsflammer.common.shared.permissions.group.GroupNames;
+import br.com.twinsflammer.common.shared.permissions.user.data.User;
+import br.com.twinsflammer.common.shared.permissions.user.manager.UserManager;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
@@ -9,9 +13,13 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 public class PlayerInteractEntityListener implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+
+        User user = UserManager.getUser(player.getUniqueId());
+
         Entity entity = event.getRightClicked();
 
-        if (entity instanceof ItemFrame)
+        if (entity.getType() == EntityType.ITEM_FRAME && !user.hasGroup(GroupNames.DIRECTOR))
             event.setCancelled(true);
     }
 }
